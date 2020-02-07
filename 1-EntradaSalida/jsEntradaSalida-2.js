61312c21127e1c53014e60b6eafd66a4ba5179cd
yamiladvargas <60760248+yamiladvargas@users.noreply.github.com>
2020-02-07 13:36:23 -0300

    Add files via upload
    del 1 al 4

---
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -0,0 +1,10 @@
+/*Debemos lograr tomar un nombre con 'prompt' 
+y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
+function mostrar()
+{
+var nombre;
+nombre= "Escriba su nombre";
+prompt("Escriba su nombre");
+alert(nombre);
+}
+
---
+++ 1-EntradaSalida/jsEntradaSalida-5.js
@@ -0,0 +1,11 @@
+/*Debemos lograr tomar nombre y edad por ID y mostrarlos concatenados 
+ej.: "Usted se llama José y tiene 66 años" 	*/
+function mostrar()
+{	
+var nombre ="nombre";
+var edad ="edad";
+resultado = "Usted se llama +"nombre"y tiene + "edad" "años");
+
+
+}
+
